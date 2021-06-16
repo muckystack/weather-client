e30484145be97821f53ceb23473268600eae5b83
code-angelo <rosillo_dev@outlook.com>
2021-06-15 22:50:55 -0500

    feat: temperatura actual

--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -2,10 +2,11 @@ import { WeatherIconComponent } from './weather-icon/weather-icon.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchLocationComponent } from './search-location/search-location.component';
+import { CurrentTempComponent } from './current-temp/current-temp.component';
 
 @NgModule({
-  declarations: [SearchLocationComponent, WeatherIconComponent],
+  declarations: [SearchLocationComponent, WeatherIconComponent, CurrentTempComponent],
   imports: [CommonModule],
-  exports: [SearchLocationComponent, WeatherIconComponent],
+  exports: [SearchLocationComponent, WeatherIconComponent, CurrentTempComponent],
 })
 export class WidgetsModule {}
