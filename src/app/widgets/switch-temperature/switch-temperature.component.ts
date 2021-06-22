500ae2d9667ef03b89a47134d8af5e2e778cc4cc
muckystack <muckystack@gmail.com>
2021-06-21 23:06:56 -0500

    FEAT: Temperature options

---
+++ src/app/widgets/switch-temperature/switch-temperature.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-switch-temperature',
+  templateUrl: './switch-temperature.component.html',
+  styleUrls: ['./switch-temperature.component.css']
+})
+export class SwitchTemperatureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchLocationComponent } from './search-location/search-location.component';
 import { CurrentTempComponent } from './current-temp/current-temp.component';
+import { SwitchTemperatureComponent } from './switch-temperature/switch-temperature.component';
 
 @NgModule({
-  declarations: [SearchLocationComponent, WeatherIconComponent, CurrentTempComponent],
+  declarations: [
+    SearchLocationComponent,
+    WeatherIconComponent,
+    CurrentTempComponent,
+    SwitchTemperatureComponent,
+  ],
   imports: [CommonModule],
-  exports: [SearchLocationComponent, WeatherIconComponent, CurrentTempComponent],
+  exports: [
+    SearchLocationComponent,
+    WeatherIconComponent,
+    CurrentTempComponent,
+    SwitchTemperatureComponent,
+  ],
 })
 export class WidgetsModule {}
