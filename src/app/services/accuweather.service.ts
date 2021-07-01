6cc0ad1f078553741dc36aa46a0655fae36ddbef
Miguel Ángel Martínez Puga <muckystack@gmail.com>
2021-06-30 23:00:58 -0500

    FEAT: Get ip

--- src/app/services/accuweather.service.ts
+++ src/app/services/accuweather.service.ts
@@ -2,18 +2,24 @@ import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Data } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccuweatherService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) { }
-
-  public getAutocompleteSearch(q:String):Observable<any>{
-
-    return this.http.get(`${environment.currentEndPoind}locations/v1/cities/autocomplete?apikey=${environment.Apikey}&q=${q}`);
-
+  public getAutocompleteSearch(q: String): Observable<any> {
+    this.getIP();
+    return this.http.get(
+      `${environment.currentEndPoind}locations/v1/cities/autocomplete?apikey=${environment.Apikey}&q=${q}`
+    );
   }
 
+  getIP() {
+    this.http.get('http://api.ipify.org/?format=json').subscribe((response) => {
+      console.log(response);
+    });
+  }
 }
